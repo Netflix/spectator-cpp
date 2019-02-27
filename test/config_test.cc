8771ac7baab11aaddfde43e182dd17fb8abbbf0a
Daniel Muino <dmuino@users.noreply.github.com>
2019-02-26 19:06:43 -0800

    Add a hook to disable/enable publishing of metrics (#11)
    We have some use cases where we want to avoid publishing metrics under
    certain conditions. Convert the Config struct into a class, and add an
    is_enabled method that can be overriden as needed. Note that the
    measurements are still taken (which will cause the meters to be reset)

---
+++ test/config_test.cc
@@ -0,0 +1,13 @@
+#include "../spectator/config.h"
+#include "../spectator/registry.h"
+#include <gtest/gtest.h>
+
+TEST(Config, Constructor) {
+  std::map<std::string, std::string> common_tags{};
+  static constexpr auto kDefault = 0;
+
+  // just make sure that our documented Config constructor works
+  spectator::Config config{common_tags, kDefault,
+                           kDefault,    kDefault,
+                           kDefault,    "http://example.org/api/v1/publish"};
+}
