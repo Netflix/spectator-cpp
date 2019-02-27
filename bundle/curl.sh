13761c2fe7c06860b59eb7407376271068754719
Daniel Muino <dmuino@users.noreply.github.com>
2019-02-27 14:35:42 -0800

    Update libcurl to 7.64.0 (#13)
    Quite a few bug fixes: https://curl.haxx.se/changes.html

--- bundle/curl.sh
+++ bundle/curl.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-curl https://curl.haxx.se/download/curl-7.53.1.tar.gz | tar zx
-cd curl-7.53.1
+curl https://curl.haxx.se/download/curl-7.64.0.tar.gz | tar zx
+cd curl-7.64.0
 ./configure  --enable-optimize --disable-dict --disable-gopher --disable-ftp --disable-imap --disable-ldap --disable-ldaps --disable-pop3 --disable-proxy --disable-rtsp --disable-smtp --disable-telnet --disable-tftp --disable-zlib --without-ca-bundle --without-gnutls --without-libidn --without-librtmp --without-libssh2 --without-nss --without-ssl --without-zlib --disable-shared  --enable-threaded-resolver
 make -j4
