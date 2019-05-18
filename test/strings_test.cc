0c25f2b970d99e738e9dce62c01bd9871bdd9c0d
Daniel Muino <dmuino@netflix.com>
2019-05-17 17:04:40 -0700

    Add ipc.endpoint tag
    Use the path from the URL to set the ipc.endpoint

--- test/strings_test.cc
+++ test/strings_test.cc
@@ -17,4 +17,14 @@ TEST(Strings, TrimRight) {
   TrimRight(&str);
   EXPECT_EQ(str, " foo bar baz");
 }
+
+TEST(Strings, PathFromUrl) {
+  using spectator::PathFromUrl;
+
+  EXPECT_EQ(PathFromUrl("http://example.com:81/foo/bar?a=b"), "/foo/bar");
+
+  EXPECT_EQ(PathFromUrl("/foo/bar"), "/foo/bar");
+  EXPECT_EQ(PathFromUrl("http://foo"), "/");
+}
+
 }  // namespace
