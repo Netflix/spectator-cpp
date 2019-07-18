f03c197b300dbd1e2a0e8a6ce25c53e42566818c
Daniel Muino <dmuino@users.noreply.github.com>
2019-07-18 16:37:26 -0700

    Simplify our HTTP client (#38)
    Instead of taking a shared pointer to a payload, just pass a pointer and
    delegate its management to the caller.

--- spectator/http_client.h
+++ spectator/http_client.h
@@ -5,40 +5,53 @@
 #include <string>
 
 #include <rapidjson/document.h>
+#include "memory.h"
 
 namespace spectator {
 
 class Registry;
 class CurlHeaders;
 
+struct HttpClientConfig {
+  std::chrono::milliseconds connect_timeout;
+  std::chrono::milliseconds read_timeout;
+  bool compress;
+  size_t json_buffer_size;
+};
+
 class HttpClient {
  public:
-  HttpClient(Registry* registry, std::chrono::milliseconds connect_timeout,
-             std::chrono::milliseconds read_timeout)
+  static constexpr const char* const kJsonType =
+      "Content-Type: application/json";
+
+  HttpClient(Registry* registry, HttpClientConfig config)
       : registry_(registry),
-        connect_timeout_(connect_timeout),
-        total_timeout_(read_timeout + connect_timeout) {}
+        config_{config},
+        json_buffer_{
+            std::unique_ptr<char[]>(new char[config.json_buffer_size])} {}
 
   int Post(const std::string& url, const char* content_type,
-           const char* payload, size_t size, bool compress = true) const;
+           const char* payload, size_t size) const;
 
   int Post(const std::string& url, const char* content_type,
-           std::shared_ptr<char> payload, size_t len, bool compress = true) const;
+           const std::string& payload) const {
+    return Post(url, content_type, payload.c_str(), payload.length());
+  };
 
-  int Post(const std::string& url, const rapidjson::Document& payload,
-           bool compress = true) const;
+  int Post(const std::string& url, const rapidjson::Document& payload) const;
 
   static void GlobalInit() noexcept;
   static void GlobalShutdown() noexcept;
 
  private:
   Registry* registry_;
-  std::chrono::milliseconds connect_timeout_;
-  std::chrono::milliseconds total_timeout_;
+  HttpClientConfig config_;
+  std::unique_ptr<char[]> json_buffer_;
 
   int do_post(const std::string& url, std::shared_ptr<CurlHeaders> headers,
-              std::shared_ptr<char> payload, size_t size,
-              int attempt_number) const;
+              const char* payload, size_t size, int attempt_number) const;
+
+  std::string payload_to_str(const rapidjson::Document& payload) const;
 };
 
 }  // namespace spectator
