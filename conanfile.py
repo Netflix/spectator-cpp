c57c8ecac2269c61874a2ee6e50bbd997e6205e4
Matthew Johnson <mjjohnson33@gmail.com>
2023-12-07 16:22:14 -0500

    remove unused dependency (#102)

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,6 @@ class SpectatorCppConan(ConanFile):
         "abseil/20230125.3",
         "asio/1.28.1",
         "backward-cpp/1.6",
-        "benchmark/1.8.3",
         "fmt/10.1.1",
         "gtest/1.14.0",
         "spdlog/1.12.0"
