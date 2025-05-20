5ba8da07517197b8b0fd7c155bef0ddf492b1db8
Matthew Johnson <matthewj@netflix.com>
2025-05-20 18:55:23 -0400

    enable additional build types for consumers

--- build.sh
+++ build.sh
@@ -47,9 +47,9 @@ fi
 if [[ ! -d $BUILD_DIR ]]; then
   echo -e "${BLUE}==== install required dependencies ====${NC}"
   if [[ "$BUILD_TYPE" == "Debug" ]]; then
-    conan install . --output-folder=$BUILD_DIR --build="*" --settings=build_type=$BUILD_TYPE --profile=./sanitized
+    conan install . --output-folder="$BUILD_DIR" --build="*" --settings=build_type="$BUILD_TYPE" --profile=./sanitized
   else
-    conan install . --output-folder=$BUILD_DIR --build=missing
+    conan install . --output-folder="$BUILD_DIR" --build=missing --settings=build_type="$BUILD_TYPE"
   fi
 fi
 
