364ecf5dab7e22534863b07d264ca5edb02a25f5
Everett Badeaux <everettc1810@gmail.com>
2026-08-05 14:02:39 -0500

    Update Deps (#139)
    * Update Deps
    
    * Bump Conan Version
    
    * Build All
    
    * Fix boost

--- conanfile.py
+++ conanfile.py
@@ -3,9 +3,18 @@ from conan import ConanFile
 class SpectatorCppConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     requires = (
-        "spdlog/1.15.0",
-        "gtest/1.14.0",
-        "boost/1.83.0",
+        "spdlog/1.17.0",
+        "gtest/1.17.0",
+        "boost/1.90.0",
     )
+    # Boost.Cobalt is disabled because b2 builds an extra boost_cobalt_io_ssl library
+    # whenever it can find OpenSSL, but the boost/1.90.0 recipe does not list that library,
+    # so package_info() aborts with "built, but were not used in any boost module". The
+    # failure depends on whether OpenSSL headers happen to be visible on the build machine,
+    # which is why it broke CI but not local Linux builds. We do not use Cobalt, and
+    # disabling it stops b2 from building cobalt/cobalt_io/cobalt_io_ssl at all.
+    default_options = {
+        "boost/*:without_cobalt": True,
+    }
     tool_requires = ()
     generators = "CMakeDeps", "CMakeToolchain"
--- libs/writer/writer_types/test_utils/udp_server/udp_server.cpp
+++ libs/writer/writer_types/test_utils/udp_server/udp_server.cpp
@@ -46,7 +46,7 @@ try
     boost::asio::io_context io_context;
 
     // Create an IPv4 socket bound to localhost (127.0.0.1) and port 1234
-    const boost::asio::ip::udp::endpoint endpoint(boost::asio::ip::address::from_string("127.0.0.1"), port);
+    const boost::asio::ip::udp::endpoint endpoint(boost::asio::ip::make_address("127.0.0.1"), port);
     boost::asio::ip::udp::socket socket(io_context, boost::asio::ip::udp::v4());
 
     try
